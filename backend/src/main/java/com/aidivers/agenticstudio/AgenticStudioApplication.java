dc7c7719ba58dd0454933983a8fde67e15410622
Stanislav Koshynskyi <stasony10@gmail.com>
2026-05-22 14:52:03 +0300

    add anotation in main

--- backend/src/main/java/com/aidivers/agenticstudio/AgenticStudioApplication.java
+++ backend/src/main/java/com/aidivers/agenticstudio/AgenticStudioApplication.java
@@ -2,6 +2,7 @@ package com.aidivers.agenticstudio;
 
 import org.springframework.boot.SpringApplication;
 
+@SpringBootApplication
 public class AgenticStudioApplication {
     public static void main(String[] args) {
         SpringApplication.run(AgenticStudioApplication.class, args);
