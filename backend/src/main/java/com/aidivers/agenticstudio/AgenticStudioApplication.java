5f1acecd6f26fd027646105dbff18f8bda7efdde
Stanislav Koshynskyi <stasony10@gmail.com>
2026-05-22 14:51:38 +0300

    add main file

--- backend/src/main/java/com/aidivers/agenticstudio/AgenticStudioApplication.java
+++ backend/src/main/java/com/aidivers/agenticstudio/AgenticStudioApplication.java
@@ -1,4 +1,9 @@
 package com.aidivers.agenticstudio;
 
+import org.springframework.boot.SpringApplication;
+
 public class AgenticStudioApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AgenticStudioApplication.class, args);
+    }
 }
